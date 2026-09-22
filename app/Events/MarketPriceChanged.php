752a44d54e6f834b48048478578333d2dfb24b9e
bardiaabasirad <bardiaabasirad@gmail.com>
2026-09-22 08:23:55 +0330

    Initial commit

---
+++ app/Events/MarketPriceChanged.php
@@ -0,0 +1,35 @@
+<?php
+
+namespace App\Events;
+
+use App\Models\MarketPrice;
+use Illuminate\Broadcasting\Channel;
+use Illuminate\Broadcasting\InteractsWithSockets;
+use Illuminate\Contracts\Broadcasting\ShouldBroadcastNow;
+use Illuminate\Foundation\Events\Dispatchable;
+use Illuminate\Queue\SerializesModels;
+
+class MarketPriceChanged implements ShouldBroadcastNow
+{
+    use Dispatchable, InteractsWithSockets, SerializesModels;
+
+    /**
+     * Create a new event instance.
+     */
+    public function __construct(public MarketPrice $marketPrice) {  }
+
+    /**
+     * Get the channels the event should broadcast on.
+     *
+     * @return array<int, \Illuminate\Broadcasting\Channel>
+     */
+    public function broadcastOn(): Channel
+    {
+        return new Channel('market-price');
+    }
+
+    public function broadcastAs(): string
+    {
+        return 'changed';
+    }
+}
--- app/Observers/MarketPriceObserver.php
+++ app/Observers/MarketPriceObserver.php
@@ -2,12 +2,13 @@
 
 namespace App\Observers;
 
+use App\Events\MarketPriceChanged;
 use App\Models\MarketPrice;
 
 class MarketPriceObserver
 {
     public function created(MarketPrice $marketPrice): void
     {
-
+        MarketPriceChanged::dispatch($marketPrice);
     }
 }
